8cb7f4aefa699b3d0b5b06f064af0371bac85c72
EzDaan <daan.vanwoudenberg08@gmail.com>
2025-03-12 13:07:21 +0100

    Startscherm

---
+++ Top Down Shooter/Assets/Beginscherm.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class Beginscherm : MonoBehaviour
+{
+    // Geef de naam van de scene die je wilt laden op
+    [SerializeField] private string SampleScene;
+
+    // Functie die je koppelt aan de knop
+    public void LoadScene()
+    {
+        // Laadt de scene met de opgegeven naam
+        SceneManager.LoadScene(SampleScene);
+    }
+}
---
+++ Top Down Shooter/Assets/Quit.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+    // Functie die de game stopt
+    public void Quiten()
+    {
+        Application.Quit();
+
+    }
+}
