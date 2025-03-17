4553c17cf9400d80255858166199d90b8f351fbb
EzDaan <daan.vanwoudenberg08@gmail.com>
2025-03-17 12:53:49 +0100

    Fixedscherm
    fixed

--- Top Down Shooter/Assets/Beginscherm.cs
+++ Top Down Shooter/Assets/Beginscherm.cs
@@ -4,12 +4,12 @@ using UnityEngine.UI;
 public class Beginscherm : MonoBehaviour
 {
     // Geef de naam van de scene die je wilt laden op
-    [SerializeField] private string SampleScene;
+    [SerializeField] private string SampleScene_Casper;
 
     // Functie die je koppelt aan de knop
     public void LoadScene()
     {
         // Laadt de scene met de opgegeven naam
-        SceneManager.LoadScene(SampleScene);
+        SceneManager.LoadScene(SampleScene_Casper);
     }
 }
