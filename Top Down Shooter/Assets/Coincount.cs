d1d3e3c7a74eb084b883381a98941c33de591cb8
EzDaan <daan.vanwoudenberg08@gmail.com>
2025-03-19 13:03:21 +0100

    fixed coin dingen
    ja

--- Top Down Shooter/Assets/Coincount.cs
+++ Top Down Shooter/Assets/Coincount.cs
@@ -1,9 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using TMPro;
-using UnityEngine.SocialPlatforms.Impl;
-public class Coinscount : MonoBehaviour
+using UnityEngine;
+
+public class coincount : MonoBehaviour
 {
     public TextMeshProUGUI coinText;
     public static int coins = 0;
